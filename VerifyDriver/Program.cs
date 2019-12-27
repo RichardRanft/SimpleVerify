83e7e95b56ce82b864922e74df6ee98694817d70
AGI\rranft <richard.ranft@scientificgames.com>
2019-12-27 12:43:43 -0800

    Added command line parser to the driver app to simplify my life

--- VerifyDriver/Program.cs
+++ VerifyDriver/Program.cs
@@ -10,21 +10,27 @@ namespace VerifyDriver
 {
     class Program
     {
+        private static CCmdParser m_parser;
+
         static void Main(string[] args)
         {
+            Dictionary<String, KeyValuePair<EArgType, bool>> argList = new Dictionary<String, KeyValuePair<EArgType, bool>>();
+            argList.Add("-path", new KeyValuePair<EArgType, bool>(EArgType.VALUE, false));
+            argList.Add("-dt", new KeyValuePair<EArgType, bool>(EArgType.VALUE, false));
+            argList.Add("-sfn", new KeyValuePair<EArgType, bool>(EArgType.FLAG, false));
+            argList.Add("-fhk", new KeyValuePair<EArgType, bool>(EArgType.FLAG, false));
+            argList.Add("-rp", new KeyValuePair<EArgType, bool>(EArgType.FLAG, false));
+            argList.Add("-so", new KeyValuePair<EArgType, bool>(EArgType.FLAG, false));
+            argList.Add("-x64", new KeyValuePair<EArgType, bool>(EArgType.FLAG, false));
+            m_parser = new CCmdParser(argList);
+            if (!m_parser.Parse(args))
+            {
+                return;
+            }
             String path = @"E:\Temp\QVNW003A\";
-            if (args.Length > 0 && (File.Exists(args[0]) || Directory.Exists(args[0])))
-                path = args[0];
-            CHashTool.Dump = true;
-            CHashTool.Expand = false;
-            CHashTool.SortByFilename = true;
-            CHashTool.LowercaseDigest = true;
-            CHashTool.SortDigest = false;
-            CHashTool.UseFileHashKeys = true;
-            CHashTool.RelativePath = true;
-            CHashTool.SortOrdinal = true;
-            CHashTool.Encode64Bit = false;
-            CHashTool.Format = EDigestFormat.InlinePairFirst;
+            if (!String.IsNullOrEmpty(m_parser.Options["-path"]))
+                path = m_parser.Options["-path"];
+            loadSettings();
             DateTime start = DateTime.Now;
             Console.WriteLine("{0} - Beginning hash of {1}", start.ToString(),  path);
             if (CHashTool.GetHash(path))
@@ -35,6 +41,43 @@ namespace VerifyDriver
             }
             else
                 Console.WriteLine("Unable to hash folder {0}", path);
+        }
+
+        private static void loadSettings()
+        {
+            CHashTool.Dump = true;
+            CHashTool.Expand = false;
+            CHashTool.LowercaseDigest = false;
+            CHashTool.SortDigest = false;
+            CHashTool.SortByFilename = !String.IsNullOrEmpty(m_parser.Options["-sfn"]);
+            CHashTool.UseFileHashKeys = !String.IsNullOrEmpty(m_parser.Options["-fhk"]);
+            CHashTool.RelativePath = !String.IsNullOrEmpty(m_parser.Options["-rp"]);
+            CHashTool.SortOrdinal = !String.IsNullOrEmpty(m_parser.Options["-so"]);
+            CHashTool.Encode64Bit = !String.IsNullOrEmpty(m_parser.Options["-x64"]);
+            if (!String.IsNullOrEmpty(m_parser.Options["-dt"]))
+            {
+                switch (m_parser.Options["-dt"].ToLower())
+                {
+                    case "ff":
+                        CHashTool.Format = EDigestFormat.FilesFirst;
+                        break;
+                    case "fl":
+                        CHashTool.Format = EDigestFormat.FilesLast;
+                        break;
+                    case "iff":
+                        CHashTool.Format = EDigestFormat.InlineFilesFirst;
+                        break;
+                    case "ifl":
+                        CHashTool.Format = EDigestFormat.InlineFilesLast;
+                        break;
+                    case "ipf":
+                        CHashTool.Format = EDigestFormat.InlinePairFirst;
+                        break;
+                    case "ipl":
+                        CHashTool.Format = EDigestFormat.InlinePairLast;
+                        break;
+                }
+            }
         }
 
         private static String digestToString(byte[] digest)
